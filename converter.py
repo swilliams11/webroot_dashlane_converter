1596e3834d5c696be3554196540a3280bb7d8ac1
swilliams11 <sean.williams@accenture.com>
2016-03-16 22:09:21 -0500

    converts webroot file to Dashlane

--- converter.py
+++ converter.py
@@ -1,4 +1,16 @@
-with open('pw.csv', 'rb') as csvfile:
-	spamreader = csv.DictReader(csvfile)
-	for row in spamreader:
-		print (row['login'], row['Password'])
+import csv
+
+#with open('C:\Users\logikalone1\Documents\TEMP\pw.csv', 'rb') as csvfile:
+csvfile = open('pw.csv', 'rb')
+f = open('pwoutput.csv', 'wb')
+writer = csv.writer(f)
+	#writer.writerows(someiterable)
+
+spamreader = csv.DictReader(csvfile)
+writer.writerow(("Website", "Name", "Login", "Login2", "Password", "Category", "Note"))
+for row in spamreader:
+	#print (row['Username'], row['Password'], row['URL'], row['Name'], row['Notes '])
+	writer.writerow((row['URL'],row['Name'], row['Username'], '',row['Password'],'',row['Notes '] ))
+
+csvfile.close()
+f.close()
